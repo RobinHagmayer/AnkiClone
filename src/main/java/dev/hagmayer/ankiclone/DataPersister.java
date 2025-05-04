816998cce7288d4c549e932f0315577cda4947c5
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-04 20:33:22 +0200

    feat: added DataPersister with basic functionality

---
+++ src/main/java/dev/hagmayer/ankiclone/DataPersister.java
@@ -0,0 +1,38 @@
+package dev.hagmayer.ankiclone;
+
+import com.fatboyindustrial.gsonjavatime.Converters;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+
+public class DataPersister {
+    private static final String DATA_FILE = "anki_data.json";
+    private final Gson gson;
+
+    public DataPersister() {
+        gson = Converters.registerLocalDate(new GsonBuilder()).setPrettyPrinting().create();
+    }
+
+    public void saveData(UserData userData) {
+        // TODO: Tests and better error handling
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATA_FILE))) {
+            gson.toJson(userData, writer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public UserData loadData() {
+        // TODO: Tests and better error handling
+        // What if someone modified the JSON and Gson can't deserialize it?
+        // What if the file is missing?
+        // Does this correctly recreate the UserData object?
+        // Do we want to bubble up the exception?
+        try (BufferedReader reader = new BufferedReader(new FileReader(DATA_FILE))) {
+            return gson.fromJson(reader, UserData.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
